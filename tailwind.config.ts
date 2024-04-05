69b842d9db5d34771c181395767a670ecc0a700f
unknown <antoine_grubert@hotmail.fr>
2024-04-05 09:11:11 +0200

    config : tailwind & fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        hind: ['var(--font-hind)'],
+        roboto: ['var(--font-roboto)'],
+        montserrat: ['var(--font-montserrat)'],
+      },
+      colors:{
+        'green': '#1DB954',
+        'black': '#191414',
+        'gray-dark': '#212121',
+        'gray-mid': '#353635 ',
+        'gray-light': '#282828',
+        'gray-font': '#B3B3B3',
+        'white-font': '#FFFFFF',
+      }
     },
   },
   plugins: [],
